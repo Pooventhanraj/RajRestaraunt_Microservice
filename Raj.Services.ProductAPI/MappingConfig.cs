15f275503c4ea573f4b2517f5470b0fcde904450
Pooventhanraj <lpooventhanraj@gmail.com>
2023-05-25 01:05:36 +0530

    Add project files.

---
+++ Raj.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Raj.Services.ProductAPI.Models;
+using Raj.Services.ProductAPI.Models.DTOs;
+
+namespace Raj.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
---
+++ Raj.Web/Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using static Raj.Web.SD;
+
+namespace Raj.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
